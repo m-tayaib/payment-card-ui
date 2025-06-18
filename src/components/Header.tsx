e0137c359ca9e8d331f81b284b6cb3e530ae3920
Muhammad Tayyab <tayyabdeveloper15@gmail.com>
2025-06-18 15:52:11 +0500

    add svg images and add header of payment component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,26 @@
+ import mastercard from "../assets/Mastercard.svg"
+ import visacard from "../assets/Visa.svg"
+ import americanexpress from "../assets/American Express.svg"
+ import discover from "../assets/Discover.svg"
+ const cardsImage : string[] = [mastercard, visacard, americanexpress, discover]
  function Header() {
   return (
-    <div>Header</div>
+    <>
+     <h1 className="text-2xl font-bold capitalize text-center">Credit Card Details</h1>
+    {/* payments cards shows  */}
+    <div className="flex justify-between items-center rounded-sm border-dashed border-[#d5d5d5] border-1 p-2 mt-4">
+     <div><h3 className="text-sm font-bold capitalize">payment <br /> method</h3></div>
+
+     {/* cards image show here */}
+
+     <div className="flex gap-2 items-center">
+      {cardsImage.map((card, index) => (
+        <img className="w-[32px] h-[32px]" src={card} alt={`card-${index}`} key={index} />
+      ))}
+     </div>
+     
+    </div>
+    </>
   )
 }
 
--- src/pages/PaymentPage.tsx
+++ src/pages/PaymentPage.tsx
@@ -1,6 +1,10 @@
+import Header from "../components/Header"
 function PaymentPage() {
   return (
-    <div>PaymentPage</div>
+    <div className="w-[328px]  h-[620px]   rounded-lg p-4 ">
+      <Header />
+
+    </div>
   )
 }
 
